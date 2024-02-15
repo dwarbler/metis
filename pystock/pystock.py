bf00c2c5e64ab205188c9b6bf73c021415b2c23a
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-02-14 18:51:32 -0600

    Chore: Add documentation

--- pystock/pystock.py
+++ pystock/pystock.py
@@ -4,6 +4,19 @@ from pandas_datareader import data as pdr
 
 
 def fetch_stock_data(ticker: str, length: int) -> pd.DataFrame:
+    """fetches pandas dataframe of stock prices for a given ticker
+
+    Args:
+        ticker (str): stock ticker
+        length (int): number of days of data to fetch
+
+    Raises:
+        TypeError: incorrect/inavlid type given to function
+        ConnectionError: invalid ticker given to function
+
+    Returns:
+        pd.DataFrame: dataframe containing stock data
+    """
     if not isinstance(ticker, str) or not isinstance(length, int) or length < 1:
         raise TypeError()
 
